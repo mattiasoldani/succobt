e81eca5833629987362adf630b9ef68443302d3f
Mattia Soldani <mattiasoldani93@gmail.com>
2024-09-14 23:18:31 +0200

    update for next beamtest

--- asciinroot_to_root_pp/asciinroot_to_root_pp_NOASCII.cc
+++ asciinroot_to_root_pp/asciinroot_to_root_pp_NOASCII.cc
@@ -27,7 +27,7 @@ std::cout<<"-->       into file "<<outFileName<<std::endl;
 
 const int nDigiCh0 = 32 ;
 const int nDigiCh1 = 32 ;
-const int nDigiCh2 = 8 ;
+const int nDigiCh2 = 0 ;
 const int nDigiCh = nDigiCh0+nDigiCh1+nDigiCh2 ;
 const int lWf0 = 1024 ;
 const int lWf1 = 1024 ;
@@ -45,6 +45,13 @@ const int lWfFast2 = 1024 ;
 const int lWfFastDummyPre = 2 ;
 const int lWfFastDummyPost = 2 ;
 
+// OPEN INPUT TEXT FILE
+std::cout<<"--> opening input ASCII"<<std::endl;
+ifstream myfile;
+string line;
+Int_t lineNumber=0;
+myfile.open(inFileName.c_str()); 
+
 // OPEN INPUT ROOT FILE(S) AND GET TREE(S)
 std::cout<<"--> opening input trees"<<std::endl;
 Int_t dummy=0;
@@ -52,7 +59,7 @@ Int_t treevt=0;
 TFile* inFileTree = new TFile(inFileNameTree.c_str());
 TTree* ntree0=(TTree*)inFileTree->Get("h3");  // #0 - 1742
 TTree* ntree1=(TTree*)inFileTree->Get("h4");  // #1 - 1742
-TTree* ntree2=(TTree*)inFileTree->Get("h2");  // #2 - 1730
+//TTree* ntree2=(TTree*)inFileTree->Get("h2");  // #2 - 1730
 
 // CREATE OUTPUT TREE AND ROOT FILE
 std::cout<<"--> opening output file"<<std::endl;
@@ -91,9 +98,9 @@ ntree1->SetBranchAddress("Idigi_742b", &t_idigi_1[0]);
 ntree1->SetBranchAddress("Idigifast_742b", &t_idigifast_1[0]);
 
 // #2 - 1730
-UShort_t t_idigi_2[(lWfDummyPre+lWf2+lWfDummyPost)*nDigiCh2];
+//UShort_t t_idigi_2[(lWfDummyPre+lWf2+lWfDummyPost)*nDigiCh2];
 
-ntree2->SetBranchAddress("Idigi_730", &t_idigi_2[0]);
+//ntree2->SetBranchAddress("Idigi_730", &t_idigi_2[0]);
 
 // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 // vvv LOOP ON ENTRIES IN TREE(S) FOR EACH EVENT IN TEXT FILE
@@ -102,7 +109,7 @@ while(treevt<ntree0->GetEntries()){  // choose the input tree from which to get 
   // GET SINGLE ENTRY
   ntree0->GetEntry(treevt);  // #0 - 1742
   ntree1->GetEntry(treevt);  // #1 - 1742
-  ntree2->GetEntry(treevt);  // #2 - 1730
+  //ntree2->GetEntry(treevt);  // #2 - 1730
   std::cout<<"--> event "<<t_eventNumber<<" in ROOT tree"<<std::endl;
 
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
@@ -185,28 +192,28 @@ while(treevt<ntree0->GetEntries()){  // choose the input tree from which to get 
   // ^^^ END OF UNPACK VARIABLES FROM TREE #1 - 1742
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-  // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-  // vvv UNPACK VARIABLES FROM TREE #2 - 1730
-  
-  // >>> STANDARD WAVEFORMS (NO FAST WAVEFORMS HERE)
-  j=0;
-  k=nDigiCh0+nDigiCh1-1;
-  for (int ii=0; ii<(lWfDummyPre+lWf2+lWfDummyPost)*nDigiCh2; ii++){
-    if( ii%(lWfDummyPre+lWf2+lWfDummyPost)==0) {
-      j=0;
-      k++;
-    }
-    if(
-    (ii%(lWfDummyPre+lWf2+lWfDummyPost))>=lWfDummyPre && 
-    (ii%(lWfDummyPre+lWf2+lWfDummyPost))<(lWfDummyPre+lWf2)
-    ){  
-      wave[k][j]=t_idigi_2[ii];
-      j++;
-    }
-  }
-  
-  // ^^^ END OF UNPACK VARIABLES FROM TREE #2 - 1730
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  //// vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+  //// vvv UNPACK VARIABLES FROM TREE #2 - 1730
+  //
+  //// >>> STANDARD WAVEFORMS (NO FAST WAVEFORMS HERE)
+  //j=0;
+  //k=nDigiCh0+nDigiCh1-1;
+  //for (int ii=0; ii<(lWfDummyPre+lWf2+lWfDummyPost)*nDigiCh2; ii++){
+  //  if( ii%(lWfDummyPre+lWf2+lWfDummyPost)==0) {
+  //    j=0;
+  //    k++;
+  //  }
+  //  if(
+  //  (ii%(lWfDummyPre+lWf2+lWfDummyPost))>=lWfDummyPre && 
+  //  (ii%(lWfDummyPre+lWf2+lWfDummyPost))<(lWfDummyPre+lWf2)
+  //  ){  
+  //    wave[k][j]=t_idigi_2[ii];
+  //    j++;
+  //  }
+  //}
+  //
+  //// ^^^ END OF UNPACK VARIABLES FROM TREE #2 - 1730
+  //// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
   // FILL OUTPUT TREE WITH THE ASSOCIATED OBJECTS IN MEMORY
   t->Fill();
